5baa2c863c30b47943dfd950e8724cb810d30d0a
Joseph <60208201+jxlove2020@users.noreply.github.com>
2026-06-30 10:22:31 +0900

    kids-play init

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = 'kids-play-v1';
+
+const ASSETS = [
+  './index.html',
+  './number-guess.html',
+  './memory-game.html',
+  './country-capital.html',
+  './manifest.json',
+  './data/countries.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
